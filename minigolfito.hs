6a09a6c87ced06d63e3eb6d3a6224c799d0070e5
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2024-12-19 13:37:59 -0300

    avance

--- minigolfito.hs
+++ minigolfito.hs
@@ -37,6 +37,16 @@ data Obstaculo = UnObstaculo {
 -}
 
 --Punto 1
+{-
+Sabemos que cada palo genera un efecto diferente, por lo tanto elegir el palo correcto puede ser la diferencia entre ganar o perder el torneo.
+Modelar los palos usados en el juego que a partir de una determinada habilidad generan un tiro que se compone por velocidad, precisión y altura.
++El putter genera un tiro con velocidad igual a 10, el doble de la precisión recibida y altura 0.
++La madera genera uno de velocidad igual a 101, altura igual a 5 y la mitad de la precisión.
++Los hierros, que varían del 1 al 10 (número al que denominaremos n), generan un tiro de velocidad igual a la fuerza multiplicada por n, la precisión dividida por n y una altura de n-3 (con mínimo 0). Modelarlos de la forma más genérica posible.
+
+Definir una constante palos que sea una lista con todos los palos que se pueden usar en el juego.
+
+-}
 data Tiro = UnTiro {
   velocidad :: Int,
   precision :: Int,
@@ -48,11 +58,53 @@ type Palo = Habilidad -> Tiro
 putter :: Palo
 putter habilidad = UnTiro {
   velocidad = 10,
-  precision = precisionJugador habilidad * 2,
+  precision = (*2).precisionJugador $ habilidad,
   altura = 0
 }
 
+madera :: Palo
+madera habilidad = UnTiro {
+  velocidad = 100,
+  precision = div (precisionJugador habilidad) 2,
+  altura = 5
+}
+
+--hierros :: Palo
+--hierros habilidad = UnTiro {
+  
+--}
+
+palos :: [Palo]
+palos = [putter, madera]
+
+--Punto 2
+{-
+Definir la función golpe que dados una persona y un palo, obtiene el tiro resultante de usar ese palo con las habilidades de la persona.
+Por ejemplo si Bart usa un putter, se genera un tiro de velocidad = 10, precisión = 120 y altura = 0.
+-}
+
 golpe :: Jugador -> Palo -> Tiro
-golpe jugador palo = palo (habilidad jugador)
+golpe jugador palo = palo.habilidad $ jugador
+
+--Punto 3
+{-
+Lo que nos interesa de los distintos obstáculos es si un tiro puede superarlo, y en el caso de poder superarlo, cómo se ve afectado dicho tiro por el obstáculo. En principio necesitamos representar los siguientes obstáculos:
+
++Un túnel con rampita sólo es superado si la precisión es mayor a 90 yendo al ras del suelo, independientemente de la velocidad del tiro. Al salir del túnel la velocidad del tiro se duplica, la precisión pasa a ser 100 y la altura 0.
+
++Una laguna es superada si la velocidad del tiro es mayor a 80 y tiene una altura de entre 1 y 5 metros. Luego de superar una laguna el tiro llega con la misma velocidad y precisión, pero una altura equivalente a la altura original dividida por el largo de la laguna.
+
++Un hoyo se supera si la velocidad del tiro está entre 5 y 20 m/s yendo al ras del suelo con una precisión mayor a 95. Al superar el hoyo, el tiro se detiene, quedando con todos sus componentes en 0.
+
+Se desea saber cómo queda un tiro luego de intentar superar un obstáculo, teniendo en cuenta que en caso de no superarlo, se detiene, quedando con todos sus componentes en 0.
+
+-}
+
+tunel :: Tiro -> Tiro
+tunel unTiro = unTiro{
+  
+}
+
+
 
 
